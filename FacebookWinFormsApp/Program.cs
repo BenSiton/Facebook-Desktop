d49588826fc64637452dc4cee698ada4c9a05c94
bensiton69 <bensiton69@gmail.com>
2021-08-27 12:50:19 +0300

    Add project files.

---
+++ FacebookWinFormsApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using FacebookWrapper;
+
+namespace FacebookApp
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Clipboard.SetText("design.patterns21cc");
+            FacebookService.s_UseForamttedToStrings = true;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormLogin());
+        }
+    }
+}
---
+++ Logic/SingletonUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FacebookWrapper.ObjectModel;
+
+namespace Logic
+{
+    public class SingletonUser
+    {
+        private SingletonUser()
+        {
+        }
+
+        public static SingletonUser Instance => sr_Instance;
+
+        protected virtual void OnUserLoggedInSuccessfully(EventArgs e)
+        {
+            UserLoggedInSuccessfully?.Invoke(this, null);
+        }
+
+        public static event EventHandler<EventArgs> UserLoggedInSuccessfully;
+
+        private static readonly SingletonUser sr_Instance = new SingletonUser();
+
+        private static User s_FacebookUser;
+
+        public static User FacebookUser
+        {
+            get => s_FacebookUser;
+            set
+            {
+                if (s_FacebookUser == null)
+                {
+                    s_FacebookUser = value;
+                    Instance.OnUserLoggedInSuccessfully(EventArgs.Empty);
+                }
+                else
+                {
+                    throw new Exception("There is already an instance!");
+                }
+            }
+        }
+    }
+}
